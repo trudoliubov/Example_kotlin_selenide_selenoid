37ec1a99d338172b59c4df04934654fdbf9df5b4
atrudoliubov <trudoliubov@outlook.com>
2021-04-12 20:20:09 +0600

    Conver java to kotlin

--- src/main/kotlin/utils/DeleteMails.kt
+++ src/main/kotlin/utils/DeleteMails.kt
@@ -1,51 +1,51 @@
-package utils;
+package utils
 
-import javax.mail.*;
-import java.util.Properties;
+import java.util.*
+import javax.mail.Flags
+import javax.mail.Folder
+import javax.mail.MessagingException
+import javax.mail.Session
 
-public class DeleteMails {
-    public void delete(String host, String port, String username, String password)
-    {
-            //create properties field
-            Properties properties = new Properties();
-
-            properties.put("mail.imap.host", host);
-            properties.put("mail.imap.port", port);
-            properties.setProperty("mail.imap.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-            properties.setProperty("mail.imap.socketFactory.fallback", "false");
-            properties.setProperty("mail.imap.socketFactory.port", String.valueOf(port));
-            Session emailSession = Session.getDefaultInstance(properties);
+class DeleteMails {
+    fun delete(host: String?, port: String, username: String?, password: String?) {
+        //create properties field
+        val properties = Properties()
+        properties["mail.imap.host"] = host
+        properties["mail.imap.port"] = port
+        properties.setProperty("mail.imap.socketFactory.class", "javax.net.ssl.SSLSocketFactory")
+        properties.setProperty("mail.imap.socketFactory.fallback", "false")
+        properties.setProperty("mail.imap.socketFactory.port", port)
+        val emailSession = Session.getDefaultInstance(properties)
         try {
             //create the POP3 store object and connect with the pop server
-            Store store = emailSession.getStore("imap");
-
-            store.connect(username, password);
+            val store = emailSession.getStore("imap")
+            store.connect(username, password)
 
             //create the folder object and open it
-            Folder emailFolder = store.getFolder("INBOX");
-            emailFolder.open(Folder.READ_WRITE);
+            val emailFolder = store.getFolder("INBOX")
+            emailFolder.open(Folder.READ_WRITE)
 
             // retrieve the messages from the folder in an array and print it
-            Message[] messages = emailFolder.getMessages();
-
-            for (int i = 0, n = messages.length; i < n; i++) {
-                Message message = messages[i];
-                if (message.getSubject().contains("Ваш код подтверждения")) {
-                    message.setFlag(Flags.Flag.DELETED, true);
+            val messages = emailFolder.messages
+            var i = 0
+            val n = messages.size
+            while (i < n) {
+                val message = messages[i]
+                if (message.subject.contains("Ваш код подтверждения")) {
+                    message.setFlag(Flags.Flag.DELETED, true)
                 }
-                if (message.getSubject().contains("Разрешение в")) {
-                    message.setFlag(Flags.Flag.DELETED, true);
+                if (message.subject.contains("Разрешение в")) {
+                    message.setFlag(Flags.Flag.DELETED, true)
                 }
-                //message.setFlag(Flags.Flag.DELETED, true);
+                i++
             }
 
             //close the store and folder objects
-            boolean expunge = true;
-            emailFolder.close(expunge);
-            store.close();
-
-        } catch (MessagingException e) {
-            e.printStackTrace();
+            val expunge = true
+            emailFolder.close(expunge)
+            store.close()
+        } catch (e: MessagingException) {
+            e.printStackTrace()
         }
     }
 }
